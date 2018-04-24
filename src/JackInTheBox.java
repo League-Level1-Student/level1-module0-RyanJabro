fef7fb5ccf12ea1862c2fb8437c619172ff92969
RyanJabro <league@WTS-iMac-11.attlocal.net>
2018-04-23 18:27:11 -0700

    JackInTheBox, SoundEffects

--- src/JackInTheBox.java
+++ src/JackInTheBox.java
@@ -1,41 +1,106 @@
+import java.applet.AudioClip;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class JackInTheBox {
-public static void main(String [] args) {
-	
-}
-private void showPicture(String fileName) { 
-    try {
-         JLabel imageLabel = createLabelImage(fileName);
-         JFrame frame = new JFrame();
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         frame.add(imageLabel);
-         frame.setVisible(true);
-         frame.pack();
-    } catch (Exception e) {
-         e.printStackTrace();
-    }
-}
-private JLabel createLabelImage(String fileName) {
-    try {
-         URL imageURL = getClass().getResource(fileName);
-         if (imageURL == null) {
-              System.err.println("Could not find image " + fileName);
-              return new JLabel();
-         } else {
-              Icon icon = new ImageIcon(imageURL);
-              JLabel imageLabel = new JLabel(icon);
-              return imageLabel;
-         }
-    } catch (Exception e) {
-         System.err.println("Could not find image " + fileName);
-         return new JLabel();
-    }
-}
+public class JackInTheBox implements MouseListener {
+	int x = 0;
 
+	public void createGui() {
+		JFrame frame = new JFrame();
+		JButton button = new JButton();
+		frame.add(button);
+		button.setText("Surprise...");
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		button.addMouseListener(this);
+		
+	}
+
+	public static void main(String[] args) {
+		JackInTheBox jitb = new JackInTheBox();
+		jitb.createGui();
+	}
+
+	private void showPicture(String fileName) {
+		try {
+			JLabel imageLabel = createLabelImage(fileName);
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private JLabel createLabelImage(String fileName) {
+		try {
+			URL imageURL = getClass().getResource(fileName);
+			if (imageURL == null) {
+				System.err.println("Could not find image " + fileName);
+				return new JLabel();
+			} else {
+				Icon icon = new ImageIcon(imageURL);
+				JLabel imageLabel = new JLabel(icon);
+				return imageLabel;
+			}
+		} catch (Exception e) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+	}
+
+	private void playSound(String soundFile) {
+		try {
+			AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+			sound.play();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("click");
+		x += 1;
+		if(x == 5) {
+			showPicture("jackInTheBox.png");
+			playSound("homer-woohoo.wav");
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
 }
--- src/SoundEffects.java
+++ src/SoundEffects.java
@@ -1,12 +1,38 @@
 import java.applet.AudioClip;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class SoundEffects {
-
+public class SoundEffects implements MouseListener{
+	JButton button = new JButton();
+	JButton button2 = new JButton();
+	JButton button3 = new JButton();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	public void createGui() {
+	panel.add(button);
+	panel.add(button2);
+	panel.add(button3);
+	frame.add(panel);
+	button.setText("Sawing Wood");
+	button2.setText("Yee");
+	button3.setText("Boom");
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.pack();
+	button.addMouseListener(this);
+	button2.addMouseListener(this);
+	button3.addMouseListener(this);
+	
+}
 	public static void main(String[] args) {
 		SoundEffects se = new SoundEffects();
-		se.playSound("sawing-wood-daniel_simon.wav");
+		se.createGui();
+		
 	}
 	
 	
@@ -15,6 +41,59 @@ public class SoundEffects {
 	private void playSound(String fileName) {
 	     AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName)); 
 	     sound.play();
+	}
+
+
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if((JButton)e.getSource() == button) {
+			playSound("sawing-wood-daniel_simon.wav");
+		}
+		else if((JButton)e.getSource() == button2) {
+			playSound("19208__wim__p1-21-yee.wav");
+		}
+		else if((JButton)e.getSource() == button3) {
+			playSound("86330__zgump__boom-1.wav");
+		}
+	}
+
+
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
